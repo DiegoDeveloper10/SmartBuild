01307f79006e4abf05415bd881f7941dabb00f0c
Diego Azevedo do Nascimento <diego.nascimento@unimedfesp.coop.br>
2018-07-18 17:14:39 -0300

    ok

---
+++ SmartBuild.Model/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartBuild.Model
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key, Column(Order = 0)]
+        [Required]
+        [DisplayName("IdUsuario")]
+        public int IdUsuario { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("NomeUsuario")]
+        public string NomeUsuario { get; set; }
+
+        [MaxLength(9)]
+        [DisplayName("Sexo")]
+        public string Sexo { get; set; }
+
+        [DisplayName("DataNascimento")]
+        [MaxLength(10)]
+        public DateTime? DataNascimento { get; set; }        
+    }
+}
